61dd1a33e348102f174f809dd20bbc88d915764f
Darrell Conklin <dmconklin@Darrells-MacBook-Pro.local>
2024-08-03 11:16:32 -0500

    Imported the state hook useState, the modules Posts and SearchBar as well as the dummyData into App.js

--- frontend/components/App/App.js
+++ frontend/components/App/App.js
@@ -5,9 +5,12 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from "../Posts/Posts"
+import SearchBar from "../SearchBar/SearchBar"
 // Import the dummyData
+import dummyData from '../../data/dummy-data';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
