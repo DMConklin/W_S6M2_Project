4cf93c091afe53caaa2a620d1cea66765cf974b7
Darrell Conklin <dmconklin@Darrells-MacBook-Pro.local>
2024-08-03 12:32:00 -0500

    created logic for likePost func, implemented Posts component in App.js and created the logic in Posts.js to display all posts using the Post component

--- frontend/components/App/App.js
+++ frontend/components/App/App.js
@@ -31,12 +31,18 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map(post => {
+      if (postID == post.id) {
+        post = {...post, likes: post.likes++}
+      }
+    }))
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts posts={posts} likePost={likePost}/>
     </div>
   );
 };
