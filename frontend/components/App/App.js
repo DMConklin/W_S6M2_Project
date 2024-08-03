632c672ebaeb8920eed3ae2201e100acb570baae
Darrell Conklin <dmconklin@Darrells-MacBook-Pro.local>
2024-08-03 11:19:50 -0500

    set state for posts and searchbar

--- frontend/components/App/App.js
+++ frontend/components/App/App.js
@@ -14,8 +14,10 @@ import dummyData from '../../data/dummy-data';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+  const [posts, setPosts] = useState(dummyData)
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [search, setSearch] = useState("")
 
   const likePost = postId => {
     /*
